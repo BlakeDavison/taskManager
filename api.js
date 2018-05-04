e810582b476a74f904006f6ffc2e0e35e07f07ee
Blake Davison <b.davison16@gmail.com>
2018-05-04 10:51:04 -0700

    refactor for DRY-ness

--- api.js
+++ api.js
@@ -9,22 +9,8 @@ module.exports = function(wagner)
   {
     return function(req, res)
     {
-      User.findOne({_id: req.params.id}, function(err, user)
-      {
-        if(err)
-        {
-          return res.
-            status(status.INTERNAL_SERVER_ERROR).
-            json({error: err.toString()});
-        }
-        if(!user)
-        {
-          return res.
-            status(status.NOT_FOUND).
-            json({error: 'Not Found'});
-        }
-        res.json({user: user});
-      });
+      User.findOne({_id: req.params.id},
+        handleOne.bind(null, 'user', res));
     };
   }));
 //set the route to get a task by ID
@@ -32,46 +18,60 @@ module.exports = function(wagner)
   {
     return function(req, res)
     {
-      Task.findOne({_id: req.params.id}, function(err, task)
-      {
-        if(err)
-        {
-          return res.
-            status(status.INTERNAL_SERVER_ERROR).
-            json({error: err.toString()});
-        }
-        if(!task)
-        {
-          return res.
-            status(status.NOT_FOUND).
-            json({error: 'Not Found'});
-        }
-        res.json({task: task});
-      });
+      Task.findOne({_id: req.params.id},
+        handleOne.bind(null, 'task', res));
     };
   }));
+//this will get the id of the person assigned to task
+/*  api.get('/task/person/:id', wagner.invoke(function()
+  {
+    return function(req, res)
+    {
+      var sort = {name:1};
+
+    };
+  }));*/
 //This will get a task by ID
   api.get('/project/id/:id', wagner.invoke(function(Project)
   {
     return function(req, res)
     {
-      Project.findOne({_id: req.params.id}, function(err, project)
-      {
-        if(err)
-        {
-          return res.
-            status(status.INTERNAL_SERVER_ERROR).
-            json({error: err.toString()});
-        }
-        if(!project)
-        {
-          return res.
-            status(status.NOT_FOUND).
-            json({error: 'Not Found'});
-        }
-        res.json({project: project});
-      });
+      Project.findOne({_id: req.params.id},
+        handleOne.bind(null, 'project', res));
     };
   }));
   return api;
 };
+
+//function definitons ti apply DRY-ness
+function handleOne(property, res, err, result)
+{
+  if(err)
+  {
+    return res.
+      status(status.INTERNAL_SERVER_ERROR).
+      json({error:err.toString()});
+  }
+  if(!result)
+  {
+    return res.
+      status(status.NOT_FOUND);
+      json({error: 'Not found'});
+  }
+  var json = {};
+  json[property] = result;
+  res.json(json);
+}
+
+function handleMany(property, res, err, result)
+{
+  if(err)
+  {
+    return res.
+      status(status.INTERNAL_SERVER_ERROR).
+      json({error: err.toString()});
+  }
+  var json = {};
+  json[property] = result;
+  res.json(json);
+}
